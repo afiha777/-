a75d90707542d756695d4623d0eca744920cf45f
afiha777 <2541586581@qq.com>
2024-09-21 14:02:44 +0800

    first commit

---
+++ 打印质数/打印质数/打印质数.cpp
@@ -0,0 +1,37 @@
+﻿#include<stdio.h>
+int main()
+{
+	int n;
+	printf("请输入一个整数");
+	scanf_s("%d", &n);
+	int b = 0;
+	for (int a = 1; a <= n; a++)
+	{
+		
+		if (n% a == 0)
+		{
+			b++;
+		}
+
+
+		printf("%d", b);
+
+	}
+	if (b == 2)
+	{
+		printf("这是一个质数");
+
+
+	}
+	else
+	{
+		printf("这不是一个质数");
+
+	}
+
+
+
+
+
+
+}
